76ffa7c2a92e725d4451f0cea4d6a53eef1fc0f7
beem420012 <beem420012@gmail.com>
2026-05-01 13:01:00 +0700

    updateEP20

--- Assets/Sc/EnemyProjectile.cs
+++ Assets/Sc/EnemyProjectile.cs
@@ -3,21 +3,35 @@ using UnityEngine;
 public class EnemyProjectile : MonoBehaviour
 {
     public float speed = 10f;
+    public int damage = 10;
+
     private Vector2 direction;
 
     public void SetDirection(Vector2 dir)
     {
         direction = dir;
 
-        // 🔥 หมุนให้ลูกไฟหันไปตามทิศ
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-
-// 👇 เพิ่ม offset
-transform.rotation = Quaternion.Euler(0, 0, angle + 180f);
+        transform.rotation = Quaternion.Euler(0, 0, angle + 180f);
     }
 
     void Update()
     {
         transform.Translate(direction * speed * Time.deltaTime, Space.World);
+    }
+
+    // 💥 👇 เพิ่มอันนี้เข้าไป
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("ชนกับ: " + other.name);
+
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("โดน Player!");
+
+            other.GetComponent<PlayerHealth>()?.TakeDamage(damage);
+
+            Destroy(gameObject);
+        }
     }
 }
